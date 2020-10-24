7265f94af72c602764c7a394104a418a58462a37
Your Name <irmantas.maciulis@gmail.com>
2020-10-24 12:12:56 +0300

    antras 2010241212

--- src/main/java/org/irmantas/tabTwo/Controllers/DarbuotojaiController.java
+++ src/main/java/org/irmantas/tabTwo/Controllers/DarbuotojaiController.java
@@ -1,14 +1,22 @@
 package org.irmantas.tabTwo.Controllers;
 
+import org.irmantas.tabTwo.model.Darbuotojas;
 import org.irmantas.tabTwo.repositories.DarbuotojasRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import reactor.core.publisher.Flux;
 
 @RequestMapping("/darb")
 public class DarbuotojaiController {
 
     @Autowired
     DarbuotojasRepository darbuotojasRepository;
+
+    @GetMapping
+    public Flux<Darbuotojas> duokDarbuotojus() {
+        return darbuotojasRepository.findAll();
+    }
 
 
 }
--- src/main/java/org/irmantas/tabTwo/config/SqlConfig.java
+++ src/main/java/org/irmantas/tabTwo/config/SqlConfig.java
@@ -5,15 +5,17 @@ import io.r2dbc.spi.ConnectionFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.r2dbc.config.AbstractR2dbcConfiguration;
+import org.springframework.data.r2dbc.repository.config.EnableR2dbcRepositories;
 
 @Configuration
+@EnableR2dbcRepositories
 public class SqlConfig  extends AbstractR2dbcConfiguration {
 
     @Override
     @Bean
     public ConnectionFactory connectionFactory() {
         return ConnectionFactories.get(
-                "r2dbcs:mysql://root:root@127.0.0.1:3306/r2dbc?" +
+                "r2dbcs:mysql://root:root@127.0.0.1:3306/tabelis?" +
                         "zeroDate=use_round&" +
                         "useSSl=false");
     }
